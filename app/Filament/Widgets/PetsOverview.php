9054b22c797e15c65b18451e66fe4062c409dcce
asekhame <asekhamejoel@gmail.com>
2024-12-22 00:47:38 +0100

    fixed migrations and added more resource and widgets

--- app/Filament/Widgets/PetsOverview.php
+++ app/Filament/Widgets/PetsOverview.php
@@ -9,28 +9,47 @@ use App\Models\owners;
 use App\Models\pets;
 use App\Models\User;
 use App\Models\Vet;
+// use Filament\Forms\Components\Card;
+use Filament\Widgets\StatsOverviewWidget\Card;
+
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
 
 class PetsOverview extends BaseWidget
 {
-    protected function getStats(): array
+    protected function getCards(): array
     {
         return [
-            Stat::make('Cats', pets::query()->where('type', 'cat')->count()),
-            Stat::make('Dogs', pets::query()->where('type', 'dog')->count()),
-            Stat::make('Rabbits', pets::query()->where('type', 'rabbit')->count()),
-            Stat::make('Owners', value: owners::query()->count()),
-            Stat::make('Vets', value: Vet::query()->count()),
-            Stat::make('Users', value: User::query()->count()),
-            Stat::make('Appointments', value: appointments::query()->count()),
-            Stat::make('Invoices', value: invoices::query()->count()),
-            Stat::make('Medical Records', value: medical_records::query()->count()),
+            Card::make('Owners', owners::query()->count())
+            ->description('Total Registered Owners')
+            ->descriptionIcon('heroicon-o-users')
+            ->color('success'),
 
 
+        Card::make('Vets', Vet::query()->count())
+            ->description('Available Veterinarians')
+            ->descriptionIcon('heroicon-o-users')
+            ->color('primary'),
 
+        Card::make('Pets', pets::query()->count())
+            ->description('Pets Registered')
+            ->descriptionIcon('heroicon-o-users')
+            ->color('info'),
 
-          
+        Card::make('Appointments', appointments::query()->count())
+            ->description('Scheduled Appointments')
+            ->descriptionIcon('heroicon-o-calendar')
+            ->color('warning'),
+
+        Card::make('Invoices', invoices::query()->count())
+            ->description('Generated Invoices')
+            ->descriptionIcon('heroicon-o-document-text')
+            ->color('secondary'),
+
+        Card::make('Medical Records', medical_records::query()->count())
+            ->description('Medical Records Logged')
+            ->descriptionIcon('heroicon-o-document-text')
+            ->color('danger')
         ];
     }
 }
--- app/Filament/Widgets/petChart.php
+++ app/Filament/Widgets/petChart.php
@@ -5,35 +5,40 @@ namespace App\Filament\Widgets;
 use App\Models\pets;
 use Filament\Widgets\ChartWidget;
 use Flowframe\Trend\Trend;
+use Filament\Widgets\Card;
+use Filament\Widgets\StatsOverviewWidget;
+use Filament\Charts\Charts\LineChart;
+use Spatie\Charts\Chart;
+use Illuminate\Support\Facades\DB;
 use Flowframe\Trend\TrendValue;
 
-class petChart extends ChartWidget
-{
-    protected static ?string $heading = 'Pets';
+// class petChart extends ChartWidget
+// {
+//     protected static ?string $heading = 'Pets';
 
-    protected function getData(): array
-    {
-        $data = Trend::model(pets::class)
-        ->between(
-            start: now()->subYear(),
-            end: now(),
-        )
-        ->perMonth()
-        ->count();
+//     protected function getData(): array
+//     {
+//         $data = Trend::model(pets::class)
+//         ->between(
+//             start: now()->subYear(),
+//             end: now(),
+//         )
+//         ->perMonth()
+//         ->count();
  
-    return [
-        'datasets' => [
-            [
-                'label' => 'Pets',
-                'data' => $data->map(fn (TrendValue $value) => $value->aggregate),
-            ],
-        ],
-        'labels' => $data->map(fn (TrendValue $value) => $value->date),
-    ];
-    }
+//     return [
+//         'datasets' => [
+//             [
+//                 'label' => 'Pets',
+//                 'data' => $data->map(fn (TrendValue $value) => $value->aggregate),
+//             ],
+//         ],
+//         'labels' => $data->map(fn (TrendValue $value) => $value->date),
+//     ];
+//     }
 
-    protected function getType(): string
-    {
-        return 'line';
-    }
-}
+//     protected function getType(): string
+//     {
+//         return 'line';
+//     }
+// }
